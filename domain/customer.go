120625a57f1e03afecddb6bfa54b5ade7b94c649
syahrul25arul <syahrul25arul@gmail.com>
2022-08-06 00:50:01 +0700

    create Customer struct

--- domain/customer.go
+++ domain/customer.go
@@ -1,5 +1,9 @@
 package domain
 
 type Customer struct {
-	
+	CustomerId  int32  `db:"customer_id"`
+	Name        string `db:"name"`
+	DateOfBirth string `db:"date_of_birth"`
+	ZipCode     string `db:"zip_code"`
+	Status      string `db:"status"`
 }
