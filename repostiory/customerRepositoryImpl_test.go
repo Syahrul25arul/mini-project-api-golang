0977fad30dbc7e73b4f98bb02962ec0dd8956b1c
syahrul25arul <syahrul25arul@gmail.com>
2022-08-06 13:06:49 +0700

    create table test for function Register in file customerRepositoryImpl.go

--- repostiory/customerRepositoryImpl_test.go
+++ repostiory/customerRepositoryImpl_test.go
@@ -4,23 +4,52 @@ import (
 	"mini-project/config"
 	"mini-project/database"
 	"mini-project/domain"
+	"mini-project/errs"
 	"testing"
 
 	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCustomerRepositoryImpl_Register(t *testing.T) {
+func TestMain(m *testing.M) {
 	godotenv.Load("./../.env")
-
 	config.SanityCheck()
+	db := database.GetClientDb()
+	db.Exec("DELETE FROM customers")
+	m.Run()
+}
 
+func TestCustomerRepositoryImpl_Register(t *testing.T) {
 	db := database.GetClientDb()
 	repository := NewCustomerRepository(db)
 
-	customer := domain.Customer{CustomerId: 2, Name: "jamal", DateOfBirth: "1995-02-25", ZipCode: "13421", Status: "inactive"}
+	// customer := domain.Customer{CustomerId: 1, Name: "jamal", DateOfBirth: "1995-02-25", ZipCode: "13421", Status: "inactive"}
 
-	err := repository.Register(customer)
+	// err := repository.Register(customer)
 
-	assert.Nil(t, err)
+	// assert.Nil(t, err)
+
+	testCase := []struct {
+		name     string
+		want     domain.Customer
+		expected *errs.AppErr
+	}{
+		{
+			name:     "Register success",
+			want:     domain.Customer{CustomerId: 1, Name: "jamal", DateOfBirth: "1995-02-25", ZipCode: "13421", Status: "inactive"},
+			expected: nil,
+		},
+		{
+			name:     "Register success",
+			want:     domain.Customer{CustomerId: 2, Name: "jamal", DateOfBirth: "1995-02-25", ZipCode: "13421"},
+			expected: errs.NewUnexpectedError("error insert data customer"),
+		},
+	}
+
+	for _, testTable := range testCase {
+		t.Run(testTable.name, func(t *testing.T) {
+			result := repository.Register(testTable.want)
+			assert.Equal(t, testTable.expected, result)
+		})
+	}
 }
